9c833869319fff6876fc3fafbcb063d6f7c3f76a
Anuj Kushwah <anujkushwah7300@gmail.com>
2026-06-23 16:15:10 +0530

    Floating action button added

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -8,8 +8,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var task = "";
-  var taskDescription = "";
+  var task = "Task 1";
+  var taskDescription = "To complete task 1 before day 1.";
 
   @override
   Widget build(BuildContext context) {
@@ -27,33 +27,314 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.green,
       ),
       body: ListView(
+        padding: EdgeInsets.only(top: 10),
         children: [
           ListTile(
-            title: Text(task),
-            subtitle: Text(taskDescription),
-            leading: IconButton(onPressed: () {}, icon: Icon(Icons.task_alt,color: Colors.blue,size: 30,)),
+            minTileHeight: 52,
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
           ),
           ListTile(
-            title: Text(task),
-            subtitle: Text(taskDescription),
-            leading: IconButton(onPressed: () {}, icon: Icon(Icons.task_alt,color: Colors.blue,size: 30,)),
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
           ),
           ListTile(
-            title: Text(task),
-            subtitle: Text(taskDescription),
-            leading: IconButton(onPressed: () {}, icon: Icon(Icons.task_alt,color: Colors.blue,size: 30,)),
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
           ),
           ListTile(
-            title: Text(task),
-            subtitle: Text(taskDescription),
-            leading: IconButton(onPressed: () {}, icon: Icon(Icons.task_alt,color: Colors.blue,size: 30,)),
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
           ),
           ListTile(
-            title: Text(task),
-            subtitle: Text(taskDescription),
-            leading: IconButton(onPressed: () {}, icon: Icon(Icons.task_alt,color: Colors.blue,size: 30,)),
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              task,
+              style: TextStyle(
+                color: Colors.green.shade900,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              taskDescription,
+              style: TextStyle(color: Colors.black45),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.task_alt, size: 30),
+              color: Colors.blue,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, size: 30),
+              color: Colors.red,
+            ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.green,
+        child: Icon(Icons.add_task, color: Colors.white, size: 36),
       ),
     );
   }
